ac55ca0acf35a5d166aee79f24a8b82b37213d51
ikram483 <ikramlabhaji2019@gmail.com>
2024-11-24 21:01:44 +0100

    documentation 1&2

---
+++ eval/eval.js
@@ -0,0 +1,27 @@
+
+// let x =10;
+// console.log(x);
+// x=20;
+// console.log(x);
+// let b =[1,2,3,4,5];
+// fo(let , i++)  
+
+// let password ="leitmein" ;
+// if (password==="leitmeine")
+// {
+//     console.log("acces")
+// }else
+// {
+//     console.log("non acces");
+// }
+// let fruit ={
+//     pomme:"vert",
+//     banane:"jaune",
+//     fraise:"rose"
+
+// };
+// console.log("la couleur et: "  );
+let b =[1,2,3,4,5];
+b.push(10);
+console.log(b);
+
