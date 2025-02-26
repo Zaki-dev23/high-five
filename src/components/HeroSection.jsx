6bfbabf2fe6c86684f88eca5a19f4a1794965acc
ikram483 <94147260+ikram483@users.noreply.github.com>
2025-02-26 19:44:07 +0100

    'HeroSection'

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,40 @@
+// import React from "react";
+// import "./HeroSection.css"; // Import du fichier CSS
+
+// const HeroSection = () => {
+//   return (
+//     <section className="hero">
+//       {/* Overlay */}
+//       <div className="hero-overlay"></div>
+
+//       {/* Contenu */}
+//       <div className="hero-content">
+//         <h1>Bienvenue sur Car Blog</h1>
+//         <p>Découvrez les dernières tendances et actualités sur l'univers de l'automobile.</p>
+//         <a href="#explore" className="hero-button">Explorer</a>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default HeroSection;
+import React from "react";
+import "./HeroSection.css"; // Import du fichier CSS
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      {/* Overlay */}
+      <div className="hero-overlay"></div>
+
+      {/* Contenu */}
+      <div className="hero-content">
+        <h1>BIENVENUE SUR OUR <span>LOCATION CAR</span> BLOG</h1>
+        <p>Découvrez les dernières tendances et actualités sur l'univers de l'automobile.</p>
+        <a href="#explore" className="hero-button">Explorer</a>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
